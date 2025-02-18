80edec32f1ac5bd74042afded1892cfc7b780e6f
ADX <ar6553@gmail.com>
2025-02-18 22:32:44 +0530

    Refactor output formatting in Week 1 tutorial example

--- Tutorial Session/Week 1/05.cpp
+++ Tutorial Session/Week 1/05.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
-    cout << "45.679" << endl;   // a. Manually adding spaces for alignment
-    cout << "3.456700e-04" << endl;  // b. Scientific notation preformatted
-    cout << "FF" << endl;            // c. Hexadecimal format manually written
-    cout << "987.6543" << endl;      // d. Fixed-point rounded manually
-    cout << "-1.2346e+02" << endl;   // e. Exponential format manually written
+    cout << setw(10) << fixed << setprecision(3) << 45.6789 << endl;   // a
+    cout << scientific << setprecision(6) << 0.00034567 << endl;      // b
+    cout << hex << uppercase << 255 << endl;                          // c
+    cout << fixed << setprecision(4) << 987.654321 << endl;           // d
+    cout << scientific << setprecision(4) << -123.456 << endl;        // e
 
     return 0;
 }
+
 
 /*
 
 Output:
 
-45.679
+    45.679
 3.456700e-04
 FF
 987.6543
 -1.2346e+02
+
 
 */
